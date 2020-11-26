685b2a82dacf7c1d7f72e6c1ca509a8ec1a14477
ShrayChona <shraychona@gmail.com>
2020-11-26 16:33:15 +0530

    Update - main activity role

--- app/src/main/java/com/shray/connectrandom/MainActivity.kt
+++ app/src/main/java/com/shray/connectrandom/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.shray.connectrandom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import com.shray.connectrandom.views.fragments.HomeFragment
+import com.shray.connectrandom.views.fragments.VideoCallFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doFragmentTransaction(R.id.flFragContainer, HomeFragment(), HomeFragment.TAG)
+    }
+
+    fun doFragmentTransaction(
+        @IdRes containerViewId: Int,
+        fragment: androidx.fragment.app.Fragment,
+        tag: String = ""
+    ) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(containerViewId, fragment, tag)
+        fragmentTransaction.commit()
     }
 }
