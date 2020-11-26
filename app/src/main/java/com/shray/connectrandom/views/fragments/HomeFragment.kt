53231eedecb7c2a090da5f3fb67cf46899afc6a0
ShrayChona <shraychona@gmail.com>
2020-11-27 00:20:17 +0530

    Add - Firestore logic added

--- app/src/main/java/com/shray/connectrandom/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/shray/connectrandom/views/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.shray.connectrandom.views.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,13 @@ import com.afollestad.assent.Permission
 import com.afollestad.assent.runWithPermissions
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.shray.connectrandom.R
 import com.shray.connectrandom.views.utils.inflate
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 /**
  * @author shraychona@gmail.com
@@ -22,6 +27,9 @@ class HomeFragment : Fragment() {
     companion object {
         const val TAG = "HomeFragment"
     }
+
+    // Variables
+    private val db by lazy { Firebase.firestore.collection("channels") }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -34,8 +42,55 @@ class HomeFragment : Fragment() {
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
 
-        runWithPermissions(Permission.RECORD_AUDIO, Permission.CAMERA) {
-
+        ibConnectCall.setOnClickListener {
+            runWithPermissions(Permission.RECORD_AUDIO, Permission.CAMERA) {
+                findChannelId()
+            }
         }
+
+    }
+
+    private fun findChannelId() {
+        val calenderTime = Calendar.getInstance()
+        calenderTime.add(Calendar.SECOND, 15)
+        val timestamp = Timestamp(Date(calenderTime.time.time))
+
+        db.whereLessThanOrEqualTo(getString(R.string.firestore_key_time_stamp), timestamp)
+            .whereEqualTo(getString(R.string.firestore_key_is_consumed), false).get()
+            .addOnSuccessListener {
+                if (it.documents.isNotEmpty()) {
+                    val document = it.documents.first()
+                    consumeChannel(document.id)
+                } else createNewChannel()
+            }.addOnFailureListener {
+                Log.d(TAG,"Fetch failed : ${it.message}")
+            }
+    }
+
+    private fun consumeChannel(documentId: String) {
+        val documentRef = db.document(documentId)
+
+        documentRef.update(getString(R.string.firestore_key_is_consumed), true)
+            .addOnSuccessListener {
+                // connect with channel id
+                Log.d(TAG,"Update success")
+            }
+            .addOnFailureListener {
+                Log.d(TAG,"update failed")
+            }
+    }
+
+    private fun createNewChannel() {
+        val newChannel = hashMapOf(
+            getString(R.string.firestore_key_is_consumed) to false,
+            getString(R.string.firestore_key_time_stamp) to Timestamp(Date())
+        )
+        db.add(newChannel)
+            .addOnSuccessListener {
+                Log.d(TAG,"Add success")
+            }
+            .addOnFailureListener {
+                Log.d(TAG,"Add failed")
+            }
     }
 }
